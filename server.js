e2aecf8b6ccfc0ad4d420eeffd128859192cc484
Asdasd7117 <alywm100@gmail.com>
2025-09-22 02:57:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,29 +5,24 @@ const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: { origin: "*" } // يسمح بالاتصال من أي جهاز
+});
 
-// يخدم index.html, room.html, css, js
-app.use(express.static(path.join(__dirname, "/"))); 
+// ملفات ثابتة
+app.use(express.static(path.join(__dirname, "/")));
 
 // -------------------- إدارة الغرف --------------------
-const rooms = {}; // تخزين الغرف مؤقتًا
-
-// ضع رابط سيرفرك هنا
-const SERVER_URL = process.env.SERVER_URL || "https://two0000-lxps.onrender.com";
+const rooms = {};
 
 app.get("/create-room", (req, res) => {
   const roomId = Math.random().toString(36).substr(2, 9);
   const token = Math.random().toString(36).substr(2, 15);
   rooms[roomId] = { token };
-
-  // رابط مطلق
-  const fullLink = `${SERVER_URL}/room.html?roomId=${roomId}&t=${token}`;
-
   res.json({
     roomId,
     token,
-    link: fullLink
+    link: `/room.html?roomId=${roomId}&t=${token}`
   });
 });
 
@@ -39,14 +34,26 @@ io.on("connection", socket => {
       socket.emit("room-error", "الغرفة غير موجودة أو التوكن غير صحيح");
       return;
     }
+
     socket.join(roomId);
+    socket.emit("joined", { selfId: socket.id, others: Object.keys(io.sockets.adapter.rooms.get(roomId) || {}).filter(id => id !== socket.id) });
     socket.to(roomId).emit("user-joined", socket.id);
 
+    // WebRTC signaling
     socket.on("offer", data => socket.to(data.to).emit("offer", { from: socket.id, sdp: data.sdp }));
     socket.on("answer", data => socket.to(data.to).emit("answer", { from: socket.id, sdp: data.sdp }));
     socket.on("candidate", data => socket.to(data.to).emit("candidate", { from: socket.id, candidate: data.candidate }));
+
+    // الدردشة
     socket.on("chat", data => io.to(roomId).emit("chat", data));
+
+    // مغادرة الغرفة
     socket.on("leave-room", () => {
+      socket.leave(roomId);
+      io.to(roomId).emit("user-left", socket.id);
+    });
+
+    socket.on("disconnect", () => {
       socket.leave(roomId);
       io.to(roomId).emit("user-left", socket.id);
     });
