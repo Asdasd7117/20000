1442d1d8e5d8e12a334335ed55fe314cadda3457
Asdasd7117 <alywm100@gmail.com>
2025-09-22 19:52:55 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+const rooms = {};
+
+io.on('connection', (socket) => {
+  console.log('User connected: ' + socket.id);
+
+  socket.on('join-room', (roomId) => {
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push(socket.id);
+
+    socket.to(roomId).emit('user-connected', socket.id);
+
+    socket.on('signal', (data) => {
+      io.to(data.to).emit('signal', { ...data, from: socket.id });
+    });
+
+    socket.on('chat-message', msg => {
+      socket.to(roomId).emit('chat-message', { user: socket.id, msg });
+    });
+
+    socket.on('disconnect', () => {
+      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+      socket.to(roomId).emit('user-disconnected', socket.id);
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
