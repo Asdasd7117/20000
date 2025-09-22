c35d8c1dea0adffd420c74dfd5996fc5f9e2cf4d
Asdasd7117 <alywm100@gmail.com>
2025-09-23 00:09:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const server = createServer(app);
@@ -9,10 +8,9 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-// لو دخل المستخدم الرابط الأساسي /
+// الصفحة الرئيسية تعرض index.html مباشرة
 app.get("/", (req, res) => {
-  const roomId = uuidv4(); // إنشاء معرف فريد للغرفة
-  res.redirect(`/room/${roomId}`);
+  res.sendFile(process.cwd() + "/public/index.html");
 });
 
 // صفحة الغرفة
@@ -20,29 +18,21 @@ app.get("/room/:roomId", (req, res) => {
   res.sendFile(process.cwd() + "/public/index.html");
 });
 
-// سوكت لإدارة الغرف
+// إدارة الغرف والمستخدمين
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId) => {
     socket.join(roomId);
     socket.to(roomId).emit("user-connected", socket.id);
 
-    // تمرير رسائل WebRTC
     socket.on("signal", (data) => {
-      io.to(data.userId).emit("signal", {
-        userId: socket.id,
-        signal: data.signal,
-      });
+      io.to(data.userId).emit("signal", { userId: socket.id, signal: data.signal });
     });
 
-    // مغادرة المستخدم
     socket.on("disconnect", () => {
       socket.to(roomId).emit("user-disconnected", socket.id);
     });
   });
 });
 
-// تشغيل السيرفر
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
