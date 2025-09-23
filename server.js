4a33867df685443481df05a8003ec2d8645f3bac
Asdasd7117 <alywm100@gmail.com>
2025-09-23 22:37:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,42 +5,38 @@ const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET","POST"] }
+});
 
 app.use(express.static("public"));
 
-// أي رابط /room/:roomId يعيد room.html
-app.get("/room/:roomId", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "room.html"));
+app.get("/room/:roomId", (req,res)=>{
+  res.sendFile(path.join(__dirname,"public","room.html"));
 });
 
-// تخزين المستخدمين في كل غرفة
 const rooms = {};
 
-io.on("connection", socket => {
+io.on("connection", socket=>{
   console.log("🔌 مستخدم متصل:", socket.id);
 
-  socket.on("join-room", roomId => {
+  socket.on("join-room", roomId=>{
     socket.join(roomId);
-    if (!rooms[roomId]) rooms[roomId] = [];
+    if(!rooms[roomId]) rooms[roomId]=[];
     rooms[roomId].push(socket.id);
 
-    // إخطار المستخدمين الآخرين
     socket.to(roomId).emit("user-joined", socket.id);
 
-    socket.on("signal", data => {
-      socket.to(data.to).emit("signal", {
-        from: socket.id,
-        signal: data.signal
-      });
+    socket.on("signal", data=>{
+      socket.to(data.to).emit("signal",{from:socket.id,signal:data.signal});
     });
 
-    socket.on("disconnect", () => {
-      console.log(`❌ المستخدم ${socket.id} خرج`);
-      rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+    socket.on("disconnect", ()=>{
+      rooms[roomId]=rooms[roomId].filter(id=>id!==socket.id);
       socket.to(roomId).emit("user-left", socket.id);
     });
   });
 });
 
-server.listen(process.env.PORT || 3000, () => console.log("🚀 السيرفر شغال"));
+server.listen(process.env.PORT||3000,()=>console.log("🚀 السيرفر شغال"));
